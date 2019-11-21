8ad700485cb67ce255d6c0abbe1ba519bc606985
Fingercomp <fingercomp@fomalhaut.me>
2019-11-21 13:55:33 +0700

    geoglasses: support OpenGlasses 2
    Closes #5

--- geoglasses/geoglasses.lua
+++ geoglasses/geoglasses.lua
@@ -24,10 +24,24 @@ local function tocolor(pl)
   end
 end
 
-function create(x,y,z,p)
-  local a=glass.addDot3D()
-  a.set3DPos(x,y,z)
-  a.setColor(table.unpack(tocolor(p)))
+local function create(x,y,z,p)
+  if glass.addDot3D then
+    -- OpenGlasses 1
+    local a = glass.addDot3D()
+    a.set3DPos(x + 0.5, y + 0.5, z + 0.5)
+    a.setColor(table.unpack(tocolor(p)))
+  else
+    -- OpenGlasses 2
+    local widget = glass.addCube3D()
+    widget.addTranslation(x + 0.125, y + 0.125, z + 0.125)
+    widget.addScale(0.75, 0.75, 0.75)
+    widget.setVisibleThroughObjects(true)
+
+    local color = tocolor(p)
+    color[4] = 1
+
+    widget.addColor(table.unpack(color))
+  end
 end
 
 for x=-size,size do
@@ -36,8 +50,10 @@ for x=-size,size do
     os.sleep(0)
     for Y=-math.min(size,18),math.min(maxY,18) do
       local y=Y+32
-      if tile[y]>pl then create(x+0.5,Y+0.5,z+0.5,tile[y]) end
+      if tile[y]>pl then create(x,Y,z,tile[y]) end
     end
   end
 end
-print("found objects:", glass.getObjectCount())
+
+
+print("found objects:", (glass.getObjectCount or glass.getWidgetCount)())
